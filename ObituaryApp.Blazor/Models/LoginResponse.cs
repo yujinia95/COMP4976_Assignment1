ccc1b8f7d16e90401c821a430d0d54dcdc560795
Yujin Jeong <76672931+yujinia95@users.noreply.github.com>
2025-10-09 13:31:29 -0700

    Delete ObituaryApp.Blazor directory

--- ObituaryApp.Blazor/Models/LoginResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace ObituaryApp.Blazor.Models;
-
-public class LoginResponse
-{
-    public string Token { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string Role { get; set; } = string.Empty;
-    public DateTime Expiration { get; set; }
-}
